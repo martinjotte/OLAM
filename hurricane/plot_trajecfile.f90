e712a37fa4bb022765bffa82cf365d1242604dcc
Martin Otte <martinjotte@gmail.com>
2023-05-04 19:01:18 +0000

    Apply Bob's Apr 29 changes.
    git-svn-id: https://svn.code.sf.net/p/olam-model/code/branches/6.0_merger@912 2bc74c4e-f164-4307-89b2-97abcff1156f

--- hurricane/plot_trajecfile.f90
+++ hurricane/plot_trajecfile.f90
@@ -26,6 +26,9 @@ subroutine plot_trajecfile(iplt)
   integer, parameter :: nmodels = 40, nhours = 50
   logical, save :: newcall = .true.
 
+  ! Set latlon100 to .true. if obs lat/lon are read in deg/100 rather than in deg/10
+  logical, parameter :: latlon100 = .true.
+
   integer, save :: kmodel
 
   integer,      allocatable, save :: mhour(:,:)
@@ -35,9 +38,6 @@ subroutine plot_trajecfile(iplt)
   character(4), allocatable, save :: model_id(:,:)
 
   real :: xpt, ypt
-
-  ! if (iplt > 1) return
-  ! return
 
   if (newcall) then
      newcall = .false.
@@ -57,11 +57,15 @@ subroutine plot_trajecfile(iplt)
 
      open(32,file='aal_olam_2022sep27_12z.dati4',status='old',form='formatted')
 
-     do 
+     do ! read and process one line of data each iteration
 
-!STD    read(32,'(a2,2x,i2,2x,i10,2x,i2,2x,a4,2x,i3,2x,i3,3x,i4,3x,i3,2x,i4)') &  ! STD read
-        read(32,'(a2,2x,i2,2x,i10,2x,i2,2x,a4,2x,i3,2x,i4,3x,i5,3x,i3,2x,i4)') &  ! lat,lon x 10
-           basin, cyclone_num, yymmddhh, technum, modelid, tau, idlat, idlon, vmax, mslp
+        if (latlon100) then
+           read(32,'(a2,2x,i2,2x,i10,2x,i2,2x,a4,2x,i3,2x,i4,3x,i5,3x,i3,2x,i4)') &
+              basin, cyclone_num, yymmddhh, technum, modelid, tau, idlat, idlon, vmax, mslp
+        else
+           read(32,'(a2,2x,i2,2x,i10,2x,i2,2x,a4,2x,i3,2x,i3,3x,i4,3x,i3,2x,i4)') &
+              basin, cyclone_num, yymmddhh, technum, modelid, tau, idlat, idlon, vmax, mslp
+        endif
 
         ! Check model id
 
@@ -75,16 +79,19 @@ subroutine plot_trajecfile(iplt)
         endif
 
         ihour = ihour + 1
-
         numhours(imodel) = ihour
 
         model_id(imodel,ihour) = modelid
         mhour   (imodel,ihour) = tau
-!STD    mlat    (imodel,ihour) =  0.1 * real(idlat)
-!STD    mlon    (imodel,ihour) = -0.1 * real(idlon)  ! + 0.01 * real(imodel - 17) ! offset for visual separation
-        mlat    (imodel,ihour) =  0.01 * real(idlat)
-        mlon    (imodel,ihour) = -0.01 * real(idlon) ! + 0.01 * real(imodel - 17) ! offset for visual separation
-                                                                                ! (OL01 is model 17)
+
+        if (latlon100) then
+           mlat(imodel,ihour) =  0.01 * real(idlat)
+           mlon(imodel,ihour) = -0.01 * real(idlon)
+        else
+           mlat(imodel,ihour) =  0.1 * real(idlat)
+           mlon(imodel,ihour) = -0.1 * real(idlon)
+        endif
+
         ! Find "earth" coordinates of sims hurricane center
 
         zeh = erad * sin(mlat(imodel,ihour) * pio180)
@@ -113,9 +120,11 @@ subroutine plot_trajecfile(iplt)
 
   call o_pcseti ('FN',4)  ! set font number to 4 (font 2 is similar but wider spacing)
   call o_pcsetr('CL',2.)  ! line width for characters
-  bsize_hour    = .010 * (op%hp2 - op%hp1)
+!  call o_pcsetr('CL',1.)  ! line width for characters
+  bsize_hour    = .007 * (op%hp2 - op%hp1)
   bsize_modelid = .012 * (op%hp2 - op%hp1)
-  call o_gslwsc(2.) ! line width
+!  call o_gslwsc(2.) ! line width for vectors
+  call o_gslwsc(1.) ! line width for vectors
 
   ! Plot hurricane tracks from other models (now all in one group; iplt is 1 only)
 
@@ -124,29 +133,36 @@ subroutine plot_trajecfile(iplt)
 
      ! Set plot line color
 
-     if     (model_id(imodel,1) == 'AVNO') then; cycle; icolor = 142 ! GFS Model Forecast
-     elseif (model_id(imodel,1) == 'AC00') then; cycle; icolor = 118 ! GFS Ensemble Control Forecast
-     elseif (model_id(imodel,1) == 'AEMN') then; cycle; icolor = 108 ! GFS Ensemble Mean Forecast
-     elseif (model_id(imodel,1) == ' CMC') then; cycle; icolor = 137 ! Canadian Global Forecast Model
-     elseif (model_id(imodel,1) == 'CEMN') then; cycle; icolor =  91 ! Canadian Ensemble Mean Forecast
-     elseif (model_id(imodel,1) == 'COTC') then; cycle; icolor = 123 ! U.S. Navy COAMPS-TC Model Forecast
-     elseif (model_id(imodel,1) == 'CTCX') then; cycle; icolor = 111 ! U.S. Navy Experimental COAMPS-TC Forecast
-     elseif (model_id(imodel,1) == 'NVGM') then; cycle; icolor = 126 ! U.S. Navy NAVGEM Model Forecast
-     elseif (model_id(imodel,1) == ' NGX') then; cycle; icolor = 113 ! U.S. Navy NOGAPS Model Forecast (deprecated)
-     elseif (model_id(imodel,1) == 'EGRR') then; cycle; icolor = 121 ! UKMET/UKX Model Forecast
-     elseif (model_id(imodel,1) == ' UKM') then; cycle; icolor = 134 ! UKMET Model
-     elseif (model_id(imodel,1) == 'UKM2') then; cycle; icolor = 105 ! UKMET Model (interpolated 12 hours)
-     elseif (model_id(imodel,1) == 'HMON') then; cycle; icolor = 120 ! Hurricanes in a Multiscale Ocean-Coupled Non-Hydrostatic...
-     elseif (model_id(imodel,1) == 'HWRF') then; cycle; icolor = 143 ! Hurricane WRF
-     elseif (model_id(imodel,1) == 'OFCL') then; cycle; icolor =  10 ! NHC Official Forecast 
-     elseif (model_id(imodel,1) == 'BEST') then; cycle;        icolor =  97 ! BEST TRACK
-     elseif (model_id(imodel,1) == 'OLAM') then;        icolor =  92 ! OLAM
-     elseif (model_id(imodel,1) == 'OL01') then; cycle;        icolor =  88 ! OLAM-1
-     elseif (model_id(imodel,1) == 'OL02') then; cycle;        icolor =  78 ! OLAM-2
-     elseif (model_id(imodel,1) == 'OL03') then; cycle;        icolor =  73 ! OLAM-3
-     elseif (model_id(imodel,1) == 'OL04') then;         icolor =  68 ! OLAM-4
-     elseif (model_id(imodel,1) == 'OL05') then; cycle;        icolor =  68 ! OLAM-5
-     endif
+!    if (model_id(imodel,1) == 'AVNO') icolor = 142 ! GFS Model Forecast
+!    if (model_id(imodel,1) == 'AC00') icolor = 118 ! GFS Ensemble Control Forecast
+!    if (model_id(imodel,1) == 'AEMN') icolor = 108 ! GFS Ensemble Mean Forecast
+!    if (model_id(imodel,1) == ' CMC') icolor = 137 ! Canadian Global Forecast Model
+!    if (model_id(imodel,1) == 'CEMN') icolor =  91 ! Canadian Ensemble Mean Forecast
+!    if (model_id(imodel,1) == 'COTC') icolor = 123 ! U.S. Navy COAMPS-TC Model Forecast
+!    if (model_id(imodel,1) == 'CTCX') icolor = 111 ! U.S. Navy Experimental COAMPS-TC Forecast
+!    if (model_id(imodel,1) == 'NVGM') icolor = 126 ! U.S. Navy NAVGEM Model Forecast
+!    if (model_id(imodel,1) == ' NGX') icolor = 113 ! U.S. Navy NOGAPS Model Forecast (deprecated)
+!    if (model_id(imodel,1) == 'EGRR') icolor = 121 ! UKMET/UKX Model Forecast
+!    if (model_id(imodel,1) == ' UKM') icolor = 134 ! UKMET Model
+!    if (model_id(imodel,1) == 'UKM2') icolor = 105 ! UKMET Model (interpolated 12 hours)
+!    if (model_id(imodel,1) == 'HMON') icolor = 120 ! Hurricanes in a Multiscale Ocean-Coupled Non-Hydrostatic...
+!    if (model_id(imodel,1) == 'HWRF') icolor = 143 ! Hurricane WRF
+!    if (model_id(imodel,1) == 'OFCL') icolor =  10 ! NHC Official Forecast 
+     if (model_id(imodel,1) == 'BEST') icolor =  97 ! BEST TRACK
+!    if (model_id(imodel,1) == 'OLAM') icolor =  92 ! OLAM
+!    if (model_id(imodel,1) == 'OL01') icolor =  88 ! OLAM-1
+!    if (model_id(imodel,1) == 'OL02') icolor =  78 ! OLAM-2
+!    if (model_id(imodel,1) == 'OL03') icolor =  73 ! OLAM-3
+!    if (model_id(imodel,1) == 'OL12') icolor =  88 ! OLAM-12
+!    if (model_id(imodel,1) == 'OL13') icolor =  78 ! OLAM-13
+!    if (model_id(imodel,1) == 'OL15') icolor =  73 ! OLAM-15
+!    if (model_id(imodel,1) == 'OL04') icolor =  88 ! OLAM-4
+!    if (model_id(imodel,1) == 'OL05') icolor =  68 ! OLAM-5
+!    if (model_id(imodel,1) == 'OL21') icolor =  83 ! OLAM-21
+     if (model_id(imodel,1) == 'OL22') icolor =  73 ! OLAM-22
+!    if (model_id(imodel,1) == 'OL23') icolor =  68 ! OLAM-23
+     if (model_id(imodel,1) == 'OL24') icolor =  83 ! OLAM-24
+     if (model_id(imodel,1) == 'OL75') icolor =  63 ! OLAM-75
 
      call o_sflush()
 
@@ -166,19 +182,21 @@ subroutine plot_trajecfile(iplt)
 
      do ihour = 1,numhours(imodel)
 
-!if (mod(mhour(imodel,ihour),6) /= 0) cycle
- if (mhour(imodel,ihour) < 18) cycle
+      ! if (mod(mhour(imodel,ihour),6) /= 0) cycle
+      ! if (mhour(imodel,ihour) < 18) cycle
 
-        if (mhour(imodel,ihour) > 42.1)  exit    ! Plot only up to a selected time
-  !     if (mhour(imodel,ihour) > 72.1)  exit    ! Plot only up to a selected time
-  !     if (mhour(imodel,ihour) > 120.1) exit    ! Plot only up to a selected time
+        if (mhour(imodel,ihour) > 36.1)  exit    ! Plot only up to a selected time
+      ! if (mhour(imodel,ihour) > 72.1)  exit    ! Plot only up to a selected time
+      ! if (mhour(imodel,ihour) > 120.1) exit    ! Plot only up to a selected time
 
-!        if (ihour == 1) then
-        if (mhour(imodel,ihour) == 18) then
+        if (ihour == 1) then
+      ! if (mhour(imodel,ihour) == 18) then
            call o_frstpt(xsims(imodel,ihour),ysims(imodel,ihour))
         else
            call o_vector(xsims(imodel,ihour),ysims(imodel,ihour))
         endif
+
+        if (mod(mhour(imodel,ihour),3) /= 0) cycle
 
         write(title,'(i3)') mhour(imodel,ihour)
         if (ihour > 1 .or. model_id(imodel,1) == 'BEST') then   ! plot 0 HRS only for BEST TRACK
